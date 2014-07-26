6aa343ad663296b18cc22dc3979efd58087e440d
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2014-07-26 15:27:30 -0700

    Improve documentation

--- src/main/java/tips/Process.java
+++ src/main/java/tips/Process.java
@@ -19,6 +19,9 @@ public interface Process<S>
    * The rates of departure from the given point (i.e. the non-zero
    * off-diagonal entries of the rate matrix).
    * 
+   * Important: the returned object will be modified, so do not 
+   * reuse the same return value across calls.
+   * 
    * @param point
    * @return
    */
